24a016f596ae9e509fa334b275065557f2defcfa
Noah <nhilaire@gaming.tech>
2023-01-24 17:37:05 +0100

    aaaa

--- App/myapp/src/component/Banner.js
+++ App/myapp/src/component/Banner.js
@@ -8,11 +8,13 @@ const Banner = (props) => {
             <iframe className={props.theme} src="https://www.youtube.com/embed/OHwrqgYP6OQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         <div className="banner-content">
-            <h2>{props.title} <img src={props.logoImage} alt="Item logo" /></h2>
-            <p className="room-description">{props.description}</p>
-        </div>
-        <div className="banner-link">
-            <Link  to='/a-propos-de-nous'>DECOUVREZ {props.name} <AiOutlineArrowRight/></Link>
+            <div className="banner-text">
+                <h2>{props.title} <img src={props.logoImage} alt="Item logo" /></h2>
+                <p className="room-description">{props.description}</p>
+            </div>
+            <div className="banner-link">
+                <Link  to='/a-propos-de-nous'>DECOUVREZ {props.name} <AiOutlineArrowRight/></Link>
+            </div>
         </div>
     </div>
 }
