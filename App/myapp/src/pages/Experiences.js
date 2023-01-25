dff880fe43a1d409183164f417ce5f1b728fc0cc
William Bailleul <wbailleul@gaming.tech>
2023-01-25 18:58:16 +0100

    cringe

--- App/myapp/src/pages/Experiences.js
+++ App/myapp/src/pages/Experiences.js
@@ -1,6 +1,13 @@
+import Footer from '../component/Footer.js';
+import CollapsibleNavbar from '../component/CollapsibleNavbar.js';
+
 const Experiences = () => {
     return (
+        <>
+        <CollapsibleNavbar/>
         <h1>News</h1>
+        <Footer/>
+        </>
     );
 }
 
