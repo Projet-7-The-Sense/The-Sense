be92132c21af38e9d21dea06ac4e692e32de7936
git config user.email <wbailleul@gaming.tech>
2023-01-25 14:33:23 +0100

    navbar almost finish

--- App/myapp/src/component/CollapsibleNavbar.js
+++ App/myapp/src/component/CollapsibleNavbar.js
@@ -5,21 +5,21 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function CollapsibleNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" >
+    <Navbar collapseOnSelect fixed='top' expand="lg" >
       <Container fluid>
-        <Navbar.Brand href="/"><div className='home_logo_holder'><img className='' href="home_logo" src='./img/logo_black.png' alt='The_sense_logo'/></div></Navbar.Brand>
+        <Navbar.Brand href="/"><div className='home_logo_holder'><img href="home_logo" src='./img/logo_black.png' alt='The_sense_logo'/></div></Navbar.Brand>
         <div className="navbar-links">
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/News">NEWS</Nav.Link>
-              <div className='black-separation-bar'>|</div>
+              <div className='vr'></div>
               <Nav.Link href="/Experiences">NOS ÉXPERIENCES</Nav.Link>
-              <div className='black-separation-bar'>|</div>
+              <div className='vr'></div>
               <Nav.Link href="/AboutUs">À PROPOS DE NOUS</Nav.Link>
-              <div className='black-separation-bar'>|</div>
+              <div className='vr'></div>
               <Nav.Link href="/Equipements">NOS ÉQUIPEMENTS</Nav.Link>
-              <div className='black-separation-bar'>|</div>
+              <div className='vr'></div>
               <Nav.Link className='login-link' href="/Login">CONNEXION</Nav.Link>
             </Nav>
           </Navbar.Collapse>
