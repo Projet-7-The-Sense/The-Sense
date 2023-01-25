877109b563ae25511a21652fa37ba56cd855e670
William Bailleul <wbailleul@gaming.tech>
2023-01-25 01:46:18 +0100

    navbar + start haut de page

--- App/myapp/src/component/CollapsibleNavbar.js
+++ App/myapp/src/component/CollapsibleNavbar.js
@@ -6,33 +6,21 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function CollapsibleNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" >
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><div className='home_logo_holder'><img className='' href="home_logo" src='./img/logo_black.png' alt='The_sense_logo'/></div></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <div>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/News">NEWS</Nav.Link>
+              <Nav.Link href="/Experiences">NOS ÉXPERIENCES</Nav.Link>
+              <Nav.Link href="/AboutUs">À PROPOS DE NOUS</Nav.Link>
+              <Nav.Link href="/Equipements">NOS ÉQUIPEMENTS</Nav.Link>
+              <Nav.Link href="/Login">CONNEXION</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
