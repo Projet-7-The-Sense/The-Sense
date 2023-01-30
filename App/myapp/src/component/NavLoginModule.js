ed623ec9b0707f399d8836aa83c2fff2497d430e
Padrox <antoinevollet.pro@gmail.com>
2023-01-30 13:05:00 +0100

    NavFix

--- App/myapp/src/component/NavLoginModule.js
+++ App/myapp/src/component/NavLoginModule.js
@@ -1,13 +1,24 @@
+/*import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { logUser } from '../api/user';
 import { Link } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { useForm } from 'react-hook-form';
-import { FaCog } from 'react-icons/fa';
-import {BsArrowRight} from 'react-icons/bs';
+
+const onSubmit = (data) => {
+  console.log(data);
+}
+
+const handleChange = () => {
+
+}
+
+const handleLogout = () => {
+  
+}
 
 const NavLoginModule = () => {
     const { user, setUser } = useContext(UserContext);
@@ -60,46 +71,85 @@ const NavLoginModule = () => {
       <Form className="login-dropdown" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="loginId">
           <div className="login-field">
-            <Form.Label className="field-title amiko-bold">Identifiant</Form.Label>
-            <Form.Control className="field-control abel" {...register("id")} type="email" placeholder="email" />
+            <Form.Label>Identifiant</Form.Label>
+            <Form.Control {...register("id")} placeHolder="email" />
           </div>
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <div className="login-field">
-            <Form.Label className="field-title amiko-bold">Mot de passe</Form.Label>
-            <Form.Control className="field-control abel" {...register("password")} type="password" placeholder="password" />
+            <Form.Label>Mot de passe</Form.Label>
+            <Form.Control {...register("password")} placeHolder="password" />
           </div>
         </Form.Group>
-
-        <NavDropdown.Divider />
-        
-        <Button as={Link} to="/mon-compte" className="account-button" variant="white" type="redirect">Créer un compte</Button>
-        <Button variant="dark" className="amiko-bold" id="login-button" type="submit">Se connecter</Button>
+        <Button variant="dark" type="submit">Connexion</Button>
       </Form>
-    ) : (
+    )
+      (<>
+      <div className="tab-content">
+        <form className="form-profile" onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Connexion</legend>
+            <div className="form-group">
+              <Row>
+                <Col lg='3'>
+                  <label htmlFor="email">Identifiant</label>
+                </Col>
+                <Col lg='9'>
+                    <input
+                    type="text"
+                    name="username"
+                    className="form-control"
+                    id="email"
+                    placeholder="mail@mail.fr"
+                    onChange={handleChange}
+                  />
+                </Col>
+              </Row>
+
+            </div>
+            <div className="form-group">
+              <Row>
+                  <Col lg='3'>
+                    <label htmlFor="password">Mot de passe</label>
+                  </Col>
+                  <Col lg='9'>
+                      <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Password"
+                      onChange={handleChange}
+                    />
+                  </Col>
+                </Row>
+            </div>
+            <div className="footer-connect">
+              <Row >
+                <Col lg={{ span: 4, offset: 3 }}>
+                  <Link className="sign" to='/mon-compte'>Créer un compte</Link>
+                </Col>
+                <Col lg='5'>
+                  <Button type='submit' variant="dark">Connexion</Button>
+                </Col>
+              </Row>
+            </div>
+            
+            
+          </fieldset>
+        </form>
+      </div>
+      </>
+    )) || (
       <>
-        <div id="hello-user" className="amiko-bold">{"Bonjour, "+user.firstname}</div>
+      <NavDropdown.Item to="/profile">Profile</NavDropdown.Item>
+      <NavDropdown.Item ><button className="btn btn-danger" onClick={handleLogout}>Déconnexion</button></NavDropdown.Item>
+      </>
+    )}
+    
+    <NavDropdown.Divider />
 
-        <NavDropdown.Divider />
-
-        <div className="account-preview-data-inline">
-          <div className="title amiko-bold">Réservation</div>
-          <Link><div className="reservation amiko">{formatedReservation(user)} <BsArrowRight /></div></Link>
-        </div>
-
-        <div className="account-preview-data-inline points">
-          <div className="title amiko-bold">Mes points</div>
-          <div className="points amiko">{formatedCoupons(9)}</div>
-        </div>
-
-        <NavDropdown.Divider />
-
-        <div className="account-preview-buttons">
-          <Button as={Link} to="/mon-compte" id="cog-holder" className="account-button" variant="white" type="redirect">Accéder aux paramètres du compte <FaCog id="cog"/></Button>
-          <Button variant="danger" className="amiko-bold" onClick={onLogout}>Se déconnecter</Button>
-        </div>
-      </>)}
     </NavDropdown>
 }
 
-export default NavLoginModule;
+export default NavLoginModule;*/
