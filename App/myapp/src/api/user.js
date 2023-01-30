419c41e5c32e7de38773aabce819f516d21f42f5
Padrox <antoinevollet.pro@gmail.com>
2023-01-30 13:13:52 +0100

    fix login

--- App/myapp/src/api/user.js
+++ App/myapp/src/api/user.js
@@ -12,6 +12,20 @@ export const getUsers = async () => {
     return users;
 }
 
+export const logUser = async (id, psw) => {
+    const response = await fetch(
+        'http://localhost:4444/user/login?id='+id+'&password='+psw, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    const user = await response.json();
+    return user;
+}
+
 export const insertUser = async (id, password, firstname, lastname, phone_number, age, reduction_points=0) => {
     const response = await fetch(
         'http://localhost:4444/user/insert', {
